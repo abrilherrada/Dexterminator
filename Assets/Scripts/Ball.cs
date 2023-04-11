e42df183f80a14b2a4a8e2d72508c6001483acf3
abrilherrada <92555648+abrilherrada@users.noreply.github.com>
2023-04-11 19:46:44 -0300

    Modify player actions
    Modify Movement script so that the player moves and rotates according to certain speeds and inputs.
    Modify Ball Shooter script to have a shooting cooldown.
    Modify Ball script so that balls are destroyed after certain time, change size with an input, and move according to the shooter's rotation.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private Vector3 size = new Vector3(x: 0.1f, y: 0.1f, z: 0.1f);
     [SerializeField] private float speed = 5f;
     [SerializeField] private Vector3 direction = new Vector3(x: 0, y: 0, z: 1f);
     [SerializeField] private int damage = 10;
+    [SerializeField] private float timeToDisappear = 3f;
+    private float timeLeftToDisappear;
+
+    private void DuplicateSize()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            size *= 2;
+            transform.localScale = size;
+        }
+    }
+
+    private void Awake()
+    {
+        timeLeftToDisappear = timeToDisappear;
+        transform.localScale = size;
+    }
 
     void Update()
     {
-        transform.position += speed * Time.deltaTime * direction;
+        timeLeftToDisappear -= Time.deltaTime;
+        if (timeLeftToDisappear <= 0)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(speed * Time.deltaTime * direction, Space.Self);
+        DuplicateSize();
     }
 }
